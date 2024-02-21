100a9d8d03914cb28ebb6490d404fe6273ece5d3
GorbachyovKS <gorbachyovKS@gmail.com>
2024-02-21 12:36:12 +0300

    platform redact

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -11,22 +11,53 @@ const Project = ({ project }) => {
     useEffect(() => {
         setWindowWidth(window.innerWidth)
     }, [])
+
+    const handlerHref = (event, path) => {
+        if (path === 'amisleak') {
+            event.preventDefault()
+            setModalVisible(true)
+        }
+    }
+
+    const [modalVisible, setModalVisible] = useState(false)
+
     return (
-        <div className={cl.project}>
-            <div className={cl.descr}>
-                <div className={cl.title}>{project.title}</div>
-                <div className={cl.subtitle}>{project.subtitle}</div>
-                <div className={cl.link}>
-                    <a href={project.path}>{project.site}</a>
+        <div className={cl.block}>
+            <div className={cl.project}>
+                <div className={cl.descr}>
+                    <div className={cl.title}>{project.title}</div>
+                    <div className={cl.subtitle}>{project.subtitle}</div>
+                    <div className={cl.link}>
+                        <a href={project.path} onClick={(event) => handlerHref(event, project.path)}>{project.site}</a>
+                    </div>
+                    <div className={cl.tools}>
+                        {project.tools.map(tool =>
+                            <div className={cl.tool}>{tool}</div>
+                        )}
+                    </div>
                 </div>
-                <div className={cl.tools}>
-                    {project.tools.map(tool =>
-                        <div className={cl.tool}>{tool}</div>
-                    )}
-                </div>
+                {windowWidth <= 768 ? <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/ipad-${project.img}.png)` }}></div> :
+                    windowWidth <= 425 ? <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/iphone-${project.img}.png)` }}></div> : <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/mac-${project.img}.png)` }}></div>
+                }
             </div>
-            {windowWidth <= 768 ? <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/ipad-${project.img}.png)` }}></div> :
-                windowWidth <= 425 ? <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/iphone-${project.img}.png)` }}></div> : <div className={cl.image} style={{ backgroundImage: `url(./assets/projects/${project.img}/mac-${project.img}.png)` }}></div>
+            {modalVisible &&
+                <div className={cl.modal}>
+                    <div className={cl.container}>
+                        <div className={cl.containerIMG}>
+                            <img src={require('../../assets/projects/p3/1.png')} alt="1" />
+                        </div>
+                        <div className={cl.containerIMG}>
+                            <img src={require('../../assets/projects/p3/2.png')} alt="2" />
+                        </div>
+                        <div className={cl.containerIMG}>
+                            <img src={require('../../assets/projects/p3/3.png')} alt="3" />
+                        </div>
+                        <div className={cl.containerIMG}>
+                            <img src={require('../../assets/projects/p3/4.png')} alt="4" />
+                        </div>
+                    </div>
+                    <button className={cl.containerButton} onClick={() => setModalVisible(false)}>close</button>
+                </div>
             }
         </div>
     )
